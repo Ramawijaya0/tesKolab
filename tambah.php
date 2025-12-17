a00dc581dd0b3972175dfc921bcee4339a115e22
Rama Wijaya <ramawjay@gmail.com>
2025-12-17 20:00:35 +0800

    rama

---
+++ tambah.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <a href="">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea amet harum voluptate labore ex non. Molestiae perferendis, atque consequuntur nobis aperiam molestias, tenetur labore incidunt cumque eveniet architecto nisi eaque?</a>
+</body>
+
+</html>
